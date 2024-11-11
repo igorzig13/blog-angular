0b52fad6911037c0808c6c555acad8d1c4dbe482
Igor Marques <igor.marques.121@ufrn.edu.br>
2024-11-11 20:55:37 -0300

    add responsive features on main page

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -1,16 +1,23 @@
 import {Component, Input} from '@angular/core';
 import {RouterLink, RouterLinkActive} from '@angular/router';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-menu-bar',
   standalone: true,
     imports: [
         RouterLink,
-        RouterLinkActive
+        RouterLinkActive,
+        NgClass
     ],
   templateUrl: './menu-bar.component.html',
   styleUrl: './menu-bar.component.css'
 })
 export class MenuBarComponent {
     @Input() logo: string = 'TI Informo';
+    isMenuOpen: boolean = false;
+
+    toggleMenu() {
+        this.isMenuOpen = !this.isMenuOpen;
+    }
 }
